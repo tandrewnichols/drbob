7250e9e1b84a21fc9e29b8781d43b47d6eb0201b
Andrew Nichols <tandrewnichols@gmail.com>
2016-10-21 08:49:56 -0400

    Switch from swig to hbs

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 var nconf = require('./lib/nconf').init();
 var _ = require('./lib/lodash').init();
 var express = require('express');
+var hbs = require('express-handlebars');
 var app = module.exports = express();
 var router = require('./router');
-var swig = require('swig');
 
 app.set('port', nconf.get('PORT'));
-app.engine('html', require('swig').renderFile);
-app.set('view engine', 'html');
-swig.setDefaults({
-  loader: swig.loaders.fs(__dirname + '/views')
-});
-app.set('views', __dirname + '/views');
-app.set('layout', 'layout');
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
+app.set('view engine', '.hbs');
 
 // Using router so I can easily remount on a different path if necessary
 app.use('/', router);
