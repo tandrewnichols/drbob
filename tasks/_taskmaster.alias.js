a676f9dc167f4b2f59c7ac8c6cdd6c04335b85ae
Andrew Nichols <tandrewnichols@gmail.com>
2017-02-24 21:52:11 -0500

    Add build steps for heroku

--- tasks/_taskmaster.alias.js
+++ tasks/_taskmaster.alias.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'default': ['copy', /*'ngtemplates:dev',*/ 'concat', 'less', 'server', 'notify:server', 'open', 'watch']
+  'default': ['copy', /*'ngtemplates:dev',*/ 'concat', 'less', 'server', 'notify:server', 'open', 'watch'],
+  build: ['copy', 'concat', 'ngAnnotate', 'uglify', 'less']
 };
